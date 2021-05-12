0be73c3715f99237d88c2872cd0cf1b5284c8285
Jim Powers <jim@corruptmemory.com>
2021-05-11 20:50:25 -0400

    Hacky attempt to get on Vulkan 1.2

---
+++ stddef.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+
+
+#ifndef __always_inline
+#define __always_inline __inline__
+#endif
+
+typedef unsigned long size_t;
+#define NULL 0
+typedef unsigned short wchar_t;
+typedef long ptrdiff_t;
---
+++ types_fixups.go
@@ -0,0 +1,3 @@
+package vulkan
+
+type AccelerationStructureTypeNV AccelerationStructureType
