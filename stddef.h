55930a90e9304dcd12fc0384d84a1b37b4062e25
Jim Powers <jim@corruptmemory.com>
2021-05-11 22:18:49 -0400

    Added in XLib, Xcb, RANDR, and Wayland stuff

--- stddef.h
+++ stddef.h
@@ -9,3 +9,11 @@ typedef unsigned long size_t;
 #define NULL 0
 typedef unsigned short wchar_t;
 typedef long ptrdiff_t;
+typedef struct xcb_connection_t xcb_connection_t;
+typedef unsigned short xcb_window_t;
+typedef unsigned short xcb_visualid_t;
+typedef struct _XDisplay Display;
+typedef unsigned long XID;
+typedef XID Window;
+typedef unsigned long VisualID;
+typedef XID RROutput;
